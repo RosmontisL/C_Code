7cc36da08cb2c2d18907cb31fcbb85d3a9e9a96b
Rosmontis <1178353332@qq.com>
2025-10-30 23:28:31 +0800

    10.30学习

---
+++ test_10_30/test_10_30/contact.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+#define MAX 100
+#define MAX_NAME 20
+#define MAX_SEX 10
+#define MAX_TELE 12
+#define MAX_ADDR 30
+//类型的声明
+//人的信息
+typedef struct PeoInfo //重命名
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+//通信录
+typedef struct Contact
+{
+	PeoInfo data[MAX];//存放人的信息
+	int count;//记录当前通讯录中实际人的个数
+}Contact;
+
+//初始化通讯录
+void InitContact(Contact* pc);
+
+//增加联系人到通讯录
+void AddContact(Contact* pc);
+
+//打印通讯录中的信息
+void ShowContact(const Contact* pc);
+
+//删除指定联系人
+void DelContact(Contact* pc);
+
+//查找指定联系人
+void SearchContact(Contact* pc);
